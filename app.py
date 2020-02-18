291fde9b01c139a07cbe74d6a6b48c9fbd757818
Christos Ferderigos <ferderigosc@gmail.com>
2020-02-18 17:58:02 -0500

    update

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+from scrape_mars import scrape_all
+import pandas as pd
+from splinter import Browser
+from bs4 import BeautifulSoup
+import requests
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars = mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_all()
+    mars.update({}, mars_data, upsert=True)
+    return redirect("http://localhost:5000/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
